e1ffabd701f2192ddad2531a4a6b531d49668078
Anu86 <anusha.sundararajan@gmail.com>
2022-08-18 18:07:32 -0400

    Added web form to collect user info

--- LCAUtility.py
+++ LCAUtility.py
@@ -57,7 +57,6 @@ st.image(image_1, width=400)
 
 st.markdown(f'<p style="color:#c5b9cd;font-size:20px;">{"Bach Gaye Re Obama (BGRO) is a sequel to the hit film Phas Gaye Re Obama (PGRO). BGRO is a fast paced, fun-filled , hilarious gangster based satirical comedy, larger in scale and scope than its prequel. The story deals with the problems faced by a maid who is ‘used’ by the powerful diplomats abroad and how her challenging their might shakes the corridors of power both in India and the US."}</p>', unsafe_allow_html=True)
 
-
 ## Table with artist details for Movie-1
 st.table(df_movie_data.iloc[0])
                   
@@ -65,12 +64,22 @@ st.table(df_movie_data.iloc[0])
 if st.button('Get Details on Movie-1 >>'):
     show_pdf('film_projects/bgro/synopsis.pdf')
 
-## SEt up image for Movie 2
+## Contribute as USD or ETH
+def collectinfo():
+    with st.form ("Collecting User Information", clear_on_submit= True):
+        full_name= st.text_input("Full Name")
+        wallet_address= st.text_input("Ethereum wallet address")
+        cash_amount= st.text_input("USD")
+        submit = st.form_submit_button("submit")
+    
+if st.button('Contribute to Movie1'):
+    collectinfo()
+    
+## Set up image for Movie 2
 image_2 = Image.open('film_projects/pgro/pgro.png')
 st.image(image_2, width=400)
 
 st.markdown(f'<p style="color:#c5b9cd;font-size:20px;">{"The movie is a comedy with satire on recession. The story revolves around a Non-resident- Indian (NRI), Om Shashtri, who lived the American dream and made it big in the US. Then one day, as it happened in America, US economy went into recession and overnight big businesses, banks, and financial institutions crashed."}</p>', unsafe_allow_html=True)
-
 
 ## Table with artist details for Movie-2
 st.table(df_movie_data.iloc[1])
@@ -78,7 +87,11 @@ st.table(df_movie_data.iloc[1])
 if st.button('Get Details on Movie-2 >>'):
    show_pdf('film_projects/pgro/synopsis.pdf')
 
-## SEet up image for movie 3
+## Contribute as USD or ETH
+if st.button('Contribute to Movie-2'):
+        collectinfo()
+        
+## Set up image for movie 3
 image_3 = Image.open('film_projects/sjsm/sjsm.png')
 st.image(image_3, width=400)
 
@@ -90,6 +103,9 @@ st.table(df_movie_data.iloc[2])
 if st.button('Get Details on Movie-3 >>'):
  show_pdf('film_projects/sjsm/synopsis.pdf')
 
+## Contribute as USD or ETH
+if st.button('Contribute to Movie-3'):
+    collectinfo()
 
     
   
